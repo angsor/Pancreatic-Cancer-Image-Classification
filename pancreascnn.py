e49fba2f71aab2ae030c67cd0d42bf6278b28ae3
Angelos Soranidis <angelossoranidis@hotmail.com>
2020-08-21 22:04:23 +0300

    Update pancreascnn.py

--- pancreascnn.py
+++ pancreascnn.py
@@ -43,7 +43,7 @@ import matplotlib.pyplot as plt
 
 plt.figure(figsize=(10, 10))
 for images, labels in train_ds.take(1):
-    for i in range(9):
+    for i in range(8):
         ax = plt.subplot(3, 3, i + 1)
         plt.imshow(images[i].numpy().astype("uint8"))
         plt.title(int(labels[i]))
